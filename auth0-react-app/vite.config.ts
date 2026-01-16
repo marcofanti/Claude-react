a487c37dcaaf9957a5d6e9ae9f46f8af7816c6b3
Marco Fanti <marco.fanti@gmail.com>
2026-01-16 13:37:15 -0500

    Added allowed hosts

--- auth0-react-app/vite.config.ts
+++ auth0-react-app/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-      port: 8080
+      port: 8080,
+      allowedHosts: ['vast-sharing-flamingo.ngrok-free.app', 'https://00-vue-login-ddp.fly.dev/']
   }
 })
